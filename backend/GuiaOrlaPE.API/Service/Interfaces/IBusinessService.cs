b600f7705c336596250052331a1845d0279cebdb
Raphaela Samille <samramalho4@gmail.com>
2026-06-11 10:35:32 -0300

    clean: limpeza de comentarios antigos

--- backend/GuiaOrlaPE.API/Service/Interfaces/IBusinessService.cs
+++ backend/GuiaOrlaPE.API/Service/Interfaces/IBusinessService.cs
@@ -1,22 +1,4 @@
-﻿// using GuiaOrlaPE.API.Models.Requests;
-// using GuiaOrlaPE.API.Models.Responses;
-
-// namespace GuiaOrlaPE.API.Service.Interfaces;
-
-// public interface IBusinessService
-// {
-//     Task<List<BusinessResponse>> GetAllAsync();
-
-//     Task<BusinessResponse?> GetByIdAsync(Guid id);
-
-//     Task<PagedResponse<BusinessResponse>> SearchAsync(SearchBusinessRequest request);
-
-//     Task<PagedResponse<BusinessResponse>> GetByUserIdAsync(Guid userId, PaginationRequest request);
-
-//     Task<BusinessResponse> CreateAsync(CreateBusinessRequest request, Guid userId);
-// }
-
-using GuiaOrlaPE.API.Models.Requests;
+﻿using GuiaOrlaPE.API.Models.Requests;
 using GuiaOrlaPE.API.Models.Responses;
 
 namespace GuiaOrlaPE.API.Service.Interfaces;
